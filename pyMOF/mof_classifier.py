ecbed2a47038651ab14513d333844a316654b9dc
str-eat <danielchemie@outlook.com>
2018-04-19 10:39:33 -0500

    create mof_classifier.py file

---
+++ pyMOF/mof_classifier.py
@@ -0,0 +1,21 @@
+'''
+Created on Mar 19, 2018
+
+@author: danie
+'''
+
+class MOFClassifier(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, data, cif, net, **kwargs):
+        '''
+        Constructor
+        '''
+        self.data = data
+        self.cif = cif
+        self.net = net
+
+"Push to git using VS Code - Daniel"
